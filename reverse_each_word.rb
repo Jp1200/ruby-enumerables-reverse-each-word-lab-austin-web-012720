6353c5f62b3d35334ff14e6bf3104ba3fe7b4c09
Jack Parker <jprkr96@gmail.com>
2020-01-15 20:00:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
 
 words = string.split(' ') 
-rev_word"
+rev_word = []
 
-words.each{|i|  i.reverse }
+rev_word << words.each{|i|  i.reverse }
 return words.join(' ').squeeze('')
 
 
